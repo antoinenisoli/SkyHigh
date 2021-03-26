5de83c3ee834ce512fefc1d08cd512d4a7b71a1e
Patrickode <prm5983@rit.edu>
2021-03-26 11:54:05 -0400

    Made crowd members move according to the grid as it bobs up and down

--- Assets/Scripts/CrowdMember.cs
+++ Assets/Scripts/CrowdMember.cs
@@ -7,7 +7,7 @@ public class CrowdMember : MonoBehaviour
     [Tooltip("The path this crowd member is following.\n" +
         "Elements are connected by a straight line.\n" +
         "[0] = start, [length - 1] = end.")]
-    [SerializeField] private Vector3[] route;
+    [SerializeField] private Transform[] route;
     [Tooltip("How fast this crowd member should move, in units per second.")]
     [SerializeField] private float speed;
 
@@ -20,8 +20,8 @@ public class CrowdMember : MonoBehaviour
         if (route.Length > 1 && nextRouteIndex < route.Length)
         {
             //Move towards the next waypoint in the route at `speed` units per second.
-            transform.position = Vector3.MoveTowards(transform.position, route[nextRouteIndex], speed * Time.deltaTime);
-            if (transform.position == route[nextRouteIndex]) { nextRouteIndex++; }
+            transform.position = Vector3.MoveTowards(transform.position, route[nextRouteIndex].position, speed * Time.deltaTime);
+            if (transform.position == route[nextRouteIndex].position) { nextRouteIndex++; }
         }
         else if (!endActionInvoked)
         {
@@ -34,7 +34,7 @@ public class CrowdMember : MonoBehaviour
     /// Gives this crowd member a new route, places this member at the start of that route, and resets relevant internal vars.
     /// </summary>
     /// <param name="newRoute">This crowd member's new route.</param>
-    public void ResetRoute(params Vector3[] newRoute)
+    public void ResetRoute(params Transform[] newRoute)
     {
         //If the route is invalid, bail out and cry about it in the console.
         if (newRoute.Length < 2)
@@ -45,7 +45,7 @@ public class CrowdMember : MonoBehaviour
 
         nextRouteIndex = 1;
         route = newRoute;
-        transform.position = newRoute[0];
+        transform.position = newRoute[0].position;
         endActionInvoked = false;
     }
 }
