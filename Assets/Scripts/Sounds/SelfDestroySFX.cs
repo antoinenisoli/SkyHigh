267b8239c59a73cd9daabfa188a89653fc1f79cf
antoinenisoli <nisoliantoine@outlook.com>
2021-03-29 18:33:46 +0200

    smoke vfx

---
+++ Assets/Scripts/Sounds/SelfDestroySFX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroySFX : MonoBehaviour
+{
+    private void Awake()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+        source.Play();
+        Destroy(gameObject, source.clip.length);
+    }
+}
