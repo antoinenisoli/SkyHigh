c4623e788bab9f755176d286c8a92ca4157e8ec3
Yannick-db <engozooyannick@gmail.com>
2021-03-29 19:40:26 +0000

    Revert "smoke vfx"
    This reverts commit 267b8239c59a73cd9daabfa188a89653fc1f79cf.

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -13,14 +13,13 @@ public class Building : MonoBehaviour
     [SerializeField] float animDuration = 2f;
     [SerializeField] int shakeVibration = 90;
     [SerializeField] float shakeStrength = 0.3f;
-    [SerializeField] protected ParticleSystem effectVFX;
-    [SerializeField] protected GameObject buildVFX;
+    [SerializeField] protected ParticleSystem fx;
     [SerializeField] protected StatType stat;
     [SerializeField] protected int resourceGain = 10;
     [Space(15)]
     [Tooltip("The positon that crowd members will enter and exit from. If null, they'll enter/exit from this object's position.")]
-    [SerializeField] Transform crowdEntryPos = null;
-    Vector3 pos;
+    [SerializeField] private Transform crowdEntryPos = null;
+    Vector3? pos;
 
     /// <summary>
     /// The transform containing the position crowd members should enter/exit from. <br/>
@@ -32,35 +31,26 @@ public class Building : MonoBehaviour
     {
         ResourceManager.Instance.ModifyStat(stat, resourceGain);
 
-        if (effectVFX)
-            effectVFX.Play();
+        if (fx)
+            fx.Play();
     }
 
     public void Build()
     {
-        pos = transform.localPosition;
-        pos.y = 0.7f;
         Sequence sequence = DOTween.Sequence();
         sequence.Append(transform.DOLocalMoveY(0.4f, animDuration));
         sequence.Play().OnComplete(FinishBuilding);
-        GameObject smoke = Instantiate(buildVFX, pos, buildVFX.transform.rotation, transform.parent);
-        smoke.transform.localPosition = pos;
 
         MainGame.Instance.ShakeCamera(animDuration, shakeStrength, shakeVibration);
     }
 
     public void Death()
     {
-        pos = transform.localPosition;
-        pos.y = 0.7f;
-        transform.DOLocalMoveY(pos.y - 4, animDuration);
+        pos = transform.position;
+        transform.DOLocalMoveY(pos.Value.y - 4, animDuration);
         MainGame.Instance.ShakeCamera(animDuration, shakeStrength, shakeVibration);
-        GameObject smoke = Instantiate(buildVFX, pos, buildVFX.transform.rotation, transform.parent);
-        smoke.transform.localPosition = pos;
-
         Destroy(gameObject, animDuration);
         EventManager.Instance.onBuildingDestroyed?.Invoke(this);
-        MainGame.Instance.AllBuildings.Remove(name + GetInstanceID());
     }
 
     void FinishBuilding()
@@ -74,11 +64,5 @@ public class Building : MonoBehaviour
     public override string ToString()
     {
         return "Earn " + resourceGain + " " + stat.ToString() + " per turn.";
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-            Death();
     }
 }
--- Assets/Scripts/CrowdManager.cs
+++ Assets/Scripts/CrowdManager.cs
@@ -118,10 +118,7 @@ public class CrowdManager : MonoBehaviour
         }
 
         ////Now that the route is constructed, wait until the cooldown on the start location is up
-        if (spawnCooldowns.ContainsKey(newRoute[0]))
-            yield return new WaitUntil(() => spawnCooldowns[newRoute[0]] <= 0);
-        else
-            yield return null;
+        yield return new WaitUntil(() => spawnCooldowns[newRoute[0]] <= 0);
 
         //Start the cooldown at this spawn position and reset the member's route in preparation for them to set out
         spawnCooldowns[newRoute[0]] = delayBetweenMemberSpawns;
